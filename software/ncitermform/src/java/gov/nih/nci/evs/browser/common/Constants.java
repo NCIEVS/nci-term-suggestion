2ff8483127f6294ce2f86b7afefc1d51c998d0a7
Kim Ong <kim.ong@nih.gov>
2018-10-15 06:37:12 -0400

    [NCITERM-800] Update CBIIT Support Email Address

--- software/ncitermform/src/java/gov/nih/nci/evs/browser/common/Constants.java
+++ software/ncitermform/src/java/gov/nih/nci/evs/browser/common/Constants.java
@@ -50,7 +50,8 @@ package gov.nih.nci.evs.browser.common;
 public class Constants {
 
     public static final String TELEPHONE = "240-276-5541 or Toll-Free: 1-888-478-4423";
-    public static final String MAIL_TO = "ncicbiit@mail.nih.gov";
+    public static final String MAIL_TO = "NCIAppSupport@nih.gov";
+
     public static final String NCICB_URL = "http://ncicb.nci.nih.gov/support";
     public static final String CDISC_MULTIPLE_TERM_REQUEST_TEMPLATE = "CDISC_Multi_Term_Request.xls";
 
